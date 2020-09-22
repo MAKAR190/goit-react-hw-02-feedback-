fe5576ee713a2ae24c9f1340632cec6706e435ec
unknown <lutskyimakar@gmail.com>
2020-09-22 22:59:51 +0300

    Updates

---
+++ precache-manifest.cd622af0fc535fc49baf3e38edd55f50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1d69039046997b275df80b9eae9ca7",
+    "url": "/goit-react-hw-02-feedback-/index.html"
+  },
+  {
+    "revision": "2e985ed3010c04c7311f",
+    "url": "/goit-react-hw-02-feedback-/static/css/main.6466f2b7.chunk.css"
+  },
+  {
+    "revision": "0c83a323a757cd696f86",
+    "url": "/goit-react-hw-02-feedback-/static/js/2.fb584c2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback-/static/js/2.fb584c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e985ed3010c04c7311f",
+    "url": "/goit-react-hw-02-feedback-/static/js/main.7445177b.chunk.js"
+  },
+  {
+    "revision": "aa10c8bb9f70f87bc3a9",
+    "url": "/goit-react-hw-02-feedback-/static/js/runtime-main.bd0e7ee7.js"
+  }
+]);
